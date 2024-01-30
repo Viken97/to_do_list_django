ca74e882825ccb18973dce2119074d15963616c4
Alexander Viken <avi032@uit.no>
2024-01-30 14:10:12 +0100

    added new url to view

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.home, name = "home"),
     path("home/", views.home, name = "home"),
     path("create/", views.create, name = "create"),
-    path("view/", views.view, name = "view")
+    path("view/", views.view, name = "view"),
+    path("view/<int:id>", views.view, name = "view")
 ]
